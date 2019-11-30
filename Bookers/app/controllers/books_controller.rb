049d6a038f49c1a32a893a1f54a2ead6743fb10b
n-path <n.ymmt15@gmail.com>
2019-11-30 17:08:05 +0000

    first commit

---
+++ Bookers/app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+  
+
+  def top
+  end
+
+  def index
+  	# 記事を全件取得
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  	@books = Book.all
+  	@book = Book.new(book_params)
+  	if @book.save
+  	    redirect_to book_path(@book.id), notice: "Book was successfully created."
+  	else
+  		render action: :index
+  	end
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  	    redirect_to book_path(book.id), notice: "Book was successfully updated."
+  	else
+  	    render action: :edit
+  	end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path, notice: "Book was successfully destroyed."
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
